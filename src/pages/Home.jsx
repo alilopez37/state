e60d3eeeb4ee3940be814da6c979a6484958a6bd
alilopez <ali.lopez37@gmail.com>
2024-06-07 00:37:51 -0600

    [ADD] Library sweetalert2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import { Helmet } from "react-helmet-async";
+import Swal from "sweetalert2";
+import '@sweetalert2/theme-bulma'
 
 function Home() {
   const [characters, setCharacters] = useState([])
+  //const [i, setI] = useState(0)
+  let i = 0
   const handleClick = (e) => {
+    Swal.fire({
+      
+      title: "Good job!",
+      text: "Upppss!!! Ocurrio un error",
+      icon: "warning"
+    });
+  };
+
+  useEffect(()=> {
     fetch("https://rickandmortyapi.com/api/character")
     .then(response => {
       if (response.ok)
@@ -13,17 +26,20 @@ function Home() {
     })
     .then(data => {
       setCharacters(data.results)
+      console.log(data.results);
     })
     .catch(error =>{
       console.log(error);
-    })
-  };
+    })  
+  },[i])
+
   return (
     <>
       <Helmet>
         <title>App Ventus - Login</title>
       </Helmet>
       <button onClick={handleClick}>Cargar datos</button>
+      <p>{i}</p>
       <div>
         <ol>
           {characters.map(item => <li>{item.name}</li>)}
